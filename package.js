4c7256c845f5d0c540d210ec5fbdcb79ae7ae357
jden <jason@denizac.org>
2014-08-24 17:21:52 -0400

    upgrade leaflet-map dep

--- package.js
+++ package.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var L = require('leaflet')
 var leafletMap = require('leaflet-map')
 
@@ -15,7 +15,7 @@ L.tileLayer('./{z}/{x}/{y}.png',{
     "maxZoom": "13"
     })
   .addTo(map)
-},{"leaflet":4,"leaflet-map":2}],2:[function(require,module,exports){
+},{"leaflet":3,"leaflet-map":2}],2:[function(require,module,exports){
 var L = require('leaflet')
 
 function leafletMap (opt) {
@@ -23,7 +23,7 @@ function leafletMap (opt) {
     defaultStyle: true
   }
   if (opt.defaultStyle) {
-    L.Icon.Default.imagePath = 'http://cdn.leafletjs.com/leaflet-0.7.3/images'
+    L.Icon.Default.imagePath = '//cdn.leafletjs.com/leaflet-0.7.3/images'
     ensureStyle()
   }
 
@@ -41,7 +41,7 @@ function ensureStyle() {
 
   var mapStyle = document.createElement('link')
   mapStyle.setAttribute('rel','stylesheet')
-  mapStyle.setAttribute('href','http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css')
+  mapStyle.setAttribute('href','//cdn.leafletjs.com/leaflet-0.7.3/leaflet.css')
   document.body.appendChild(mapStyle)
 }
 
@@ -9227,6 +9227,4 @@ L.Map.include({
 
 
 }(window, document));
-},{}],4:[function(require,module,exports){
-module.exports=require(3)
-},{}]},{},[1])
+},{}]},{},[1]);
